e44213ae74d907af59a0df01454ce40937f204c6
Sonu Kumar <1009ukumar@gmail.com>
2022-07-15 23:32:06 +0900

    Update conf.py

--- conf.py
+++ conf.py
@@ -75,8 +75,8 @@ extensions = [
  "sphinx_rtd_theme",
 ]
 
-#html_theme = "sphinx_rtd_theme"
-html_theme = "sphinx_book_theme"
+html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_book_theme"
 
 
 html_logo = 'esta_logo2.6.0_cropped.png'
